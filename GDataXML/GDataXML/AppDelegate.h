2f48798142a3275e4bcf92f3a500aa2e828e2477
Eric <barryss@qq.com>
2016-10-03 21:16:28 +0800

    First commit master.

---
+++ GDataXML/GDataXML/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GDataXML
+//
+//  Created by Li Zhe on 10/3/16.
+//  Copyright © 2016 SH10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GDataXML/GDataXML/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GDataXML
+//
+//  Created by Li Zhe on 10/3/16.
+//  Copyright © 2016 SH10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
